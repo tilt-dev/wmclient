tilt-dev/mish

analytics: request to stats server (#1955)
GitOrigin-RevId: f7570d0894dd14d7824b4fd0bd895c4b1d06d730

cli/analytics/opt.go
@@ -1,0 +1,57 @@
+package analytics
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/windmilleng/mish/cli/dirs"
+)
+
+type AnalyticsOpt int
+
+const (
+	AnalyticsOptDefault AnalyticsOpt = iota
+	AnalyticsOptOut
+	AnalyticsOptIn
+)
+
+var choices = map[AnalyticsOpt]string{
+	AnalyticsOptDefault: "default",
+	AnalyticsOptOut:     "opt-out",
+	AnalyticsOptIn:      "opt-in",
+}
+
+func readChoiceFile() (string, error) {
+	d, err := dirs.UseWindmillDir()
+	if err != nil {
+		return "", err
+	}
+
+	txt, err := d.ReadFile(choiceFile)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return "", err
+		}
+		txt = ""
+	}
+
+	return strings.TrimSpace(txt), nil
+}
+
+func optedIn() bool {
+	txt, err := readChoiceFile()
+	if txt != "" {
+		switch txt {
+		case choices[AnalyticsOptOut]:
+			return false
+		case choices[AnalyticsOptIn]:
+			return true
+		}
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "analytics.optedIn: %v\n", err)
+	}
+	return optInByDefault
+}

tilt-dev/wmclient

pkg/analytics/opt.go
@@ -1,0 +1,57 @@
+package analytics
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/windmilleng/wmclient/pkg/dirs"
+)
+
+type AnalyticsOpt int
+
+const (
+	AnalyticsOptDefault AnalyticsOpt = iota
+	AnalyticsOptOut
+	AnalyticsOptIn
+)
+
+var choices = map[AnalyticsOpt]string{
+	AnalyticsOptDefault: "default",
+	AnalyticsOptOut:     "opt-out",
+	AnalyticsOptIn:      "opt-in",
+}
+
+func readChoiceFile() (string, error) {
+	d, err := dirs.UseWindmillDir()
+	if err != nil {
+		return "", err
+	}
+
+	txt, err := d.ReadFile(choiceFile)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return "", err
+		}
+		txt = ""
+	}
+
+	return strings.TrimSpace(txt), nil
+}
+
+func optedIn() bool {
+	txt, err := readChoiceFile()
+	if txt != "" {
+		switch txt {
+		case choices[AnalyticsOptOut]:
+			return false
+		case choices[AnalyticsOptIn]:
+			return true
+		}
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "analytics.optedIn: %v\n", err)
+	}
+	return optInByDefault
+}